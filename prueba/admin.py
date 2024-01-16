2576bec84452b929d24d39087f56d56af0db1af5
dianfer19 <dianferbj@gmail.com>
2024-01-15 23:34:14 -0500

    Crm modelos

--- prueba/admin.py
+++ prueba/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from prueba.models import Empleado, Cargos
+from prueba.models import Empleado, Cargos, Clientes, Etapas, TipoActividad, PlanActividad, DetallePlanActividad, \
+    Oportunidades, DetalleFlujoCrm, FlujoCrm
 
 
 # Register your models here.
 @admin.register(Empleado)
 class EmpleadoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'apellido',  'fecha_nacimiento', 'estado']
+    list_display = ['nombre', 'apellido', 'fecha_nacimiento', 'estado']
     search_fields = ['nombre', 'apellido', ]
     list_filter = ['fecha_nacimiento', 'estado', ]
 
@@ -16,3 +17,54 @@ class CargoAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'estado']
     search_fields = ['nombre', 'estado', ]
     list_filter = ['estado', ]
+
+
+@admin.register(Etapas)
+class EtapaAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'estado']
+    search_fields = ['nombre', 'estado', ]
+    list_filter = ['estado', ]
+
+
+@admin.register(Clientes)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['razon_social', 'ruc', 'estado']
+    search_fields = ['razon_social', 'ruc', ]
+    list_filter = ['estado', ]
+
+
+@admin.register(TipoActividad)
+class TipoActividadAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'estado']
+    search_fields = ['nombre', ]
+    list_filter = ['estado', ]
+
+
+class DetallePlanActividadInline(admin.TabularInline):
+    model = DetallePlanActividad
+
+
+@admin.register(PlanActividad)
+class TipoActividadAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'estado']
+    search_fields = ['nombre', ]
+    list_filter = ['estado', ]
+    inlines = [DetallePlanActividadInline]
+
+
+@admin.register(Oportunidades)
+class OportunidadesAdmin(admin.ModelAdmin):
+    list_display = ['descripcion', 'ingreso_esperado', 'cliente']
+    search_fields = ['descripcion', ]
+    list_filter = ['estado', ]
+
+
+class DetalleFlujoCrmInline(admin.TabularInline):
+    model = DetalleFlujoCrm
+
+
+@admin.register(FlujoCrm)
+class TipoActividadAdmin(admin.ModelAdmin):
+    list_display = ['id', 'oportunidad']
+    list_filter = ['estado', ]
+    inlines = [DetalleFlujoCrmInline]
--- prueba/models.py
+++ prueba/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 
@@ -8,6 +10,7 @@ class Cargos(models.Model):
 
     def __str__(self):
         return self.nombre
+
     class Meta:
         verbose_name = 'Cargo'
         verbose_name_plural = 'Cargos'
@@ -18,11 +21,164 @@ class Empleado(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     fecha_nacimiento = models.DateField()
-    cargo = models.ForeignKey(Cargos,blank=True, null=True, to_field='id', related_name='fk_cargo_empleado',
+    cargo = models.ForeignKey(Cargos, blank=True, null=True, to_field='id', related_name='fk_cargo_empleado',
                               on_delete=models.DO_NOTHING, )
     estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
 
     class Meta:
         verbose_name = 'Empleado'
         verbose_name_plural = 'Empleados'
         db_table = 'crm_empleado'
+
+
+class Etapas(models.Model):
+    nombre = models.CharField(max_length=50)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Etapa'
+        verbose_name_plural = 'Etapas'
+        db_table = 'crm_etapa'
+
+
+class Clientes(models.Model):
+    razon_social = models.CharField(max_length=50)
+    ruc = models.CharField(max_length=50)
+    mail = models.EmailField()
+    direccion = models.CharField(max_length=50)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.razon_social
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        db_table = 'crm_cliente'
+
+
+class TipoActividad(models.Model):
+    nombre = models.CharField(max_length=50)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Tipo de actividad'
+        verbose_name_plural = 'Tipo de actividades'
+        db_table = 'crm_tipo_actividad'
+
+
+class PlanActividad(models.Model):
+    nombre = models.CharField(max_length=50)
+    etapa = models.ForeignKey(Etapas, blank=True, null=True, to_field='id',
+                              related_name='fk_etapa_plan',
+                              on_delete=models.DO_NOTHING, )
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Plan Actividad'
+        verbose_name_plural = 'Plan Actividades'
+        db_table = 'crm_plan_actividad'
+
+
+class DetallePlanActividad(models.Model):
+    notas = [
+        ('A', 'Reunión Obligatoria'),
+        ('B', 'Reunión Virtual'),
+        ('C', 'No necesita Reunión'), ]
+    descripcion = models.CharField(max_length=200)
+    plan_actividad = models.ForeignKey(PlanActividad, blank=True, null=True, to_field='id',
+                                       related_name='fk_plan_detalle',
+                                       on_delete=models.DO_NOTHING, )
+    tipo_actividad = models.ForeignKey(TipoActividad, blank=True, null=True, to_field='id',
+                                       related_name='fk_tipo_actividad_detalle',
+                                       on_delete=models.DO_NOTHING, )
+    nota = models.CharField(max_length=1, choices=notas)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.tipo_actividad.nombre
+
+    class Meta:
+        verbose_name = 'Detalle de Plan de Actividad'
+        verbose_name_plural = 'Detalle de Plan de Actividades'
+        db_table = 'crm_detalle_plan_actividad'
+
+
+class Oportunidades(models.Model):
+    descripcion = models.CharField(max_length=200, blank=False, null=False)
+    ingreso_esperado = models.DecimalField(max_digits=10, decimal_places=2, )
+    cliente = models.ForeignKey(Clientes, blank=True, null=True, to_field='id',
+                                related_name='fk_cliente_oportunidad',
+                                on_delete=models.DO_NOTHING, )
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.cliente.razon_social
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            super(Oportunidades, self).save()
+        else:
+            super(Oportunidades, self).save()
+            vo = FlujoCrm()
+            vo.oportunidad = self
+            vo.estado = True
+            vo.save()
+            vo_detalle = DetalleFlujoCrm()
+            vo_detalle.flujo_crm = vo
+            vo_detalle.etapa = Etapas.objects.filter(id=1).first()
+            vo_detalle.save()
+
+    class Meta:
+        verbose_name = 'Oportunidad'
+        verbose_name_plural = 'Oportunidades'
+        db_table = 'crm_oportunidad'
+
+
+class FlujoCrm(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    oportunidad = models.OneToOneField(Oportunidades, blank=False, null=False, to_field='id',
+                                       related_name='fk_oprtunidad_flujo',
+                                       on_delete=models.DO_NOTHING, )
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.oportunidad.descripcion
+
+    class Meta:
+        verbose_name = 'Flujo CRM'
+        verbose_name_plural = 'Flujos CRM'
+        db_table = 'crm_flujo'
+
+
+class DetalleFlujoCrm(models.Model):
+    flujo_crm = models.ForeignKey(FlujoCrm, blank=False, null=False, to_field='id',
+                                  related_name='fk_plan_detalle_flujo',
+                                  on_delete=models.DO_NOTHING, )
+    empleado = models.ForeignKey(Empleado, blank=True, null=True, to_field='id',
+                                 related_name='fk_empleado_detalle_flujo',
+                                 on_delete=models.DO_NOTHING, )
+    etapa = models.ForeignKey(Etapas, blank=True, null=True, to_field='id',
+                              related_name='fk_etapa_detalle_flujo',
+                              on_delete=models.DO_NOTHING, )
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.etapa.nombre
+
+    class Meta:
+        verbose_name = 'Detalle de Flujo'
+        verbose_name_plural = 'Detalles de Flujo'
+        db_table = 'crm_detalle_flujo'
